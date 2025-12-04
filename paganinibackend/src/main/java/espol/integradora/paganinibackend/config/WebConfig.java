4474eaceb718d1a9bfa8a922a9c1ff3a13d66a1b
jojusuar <jojusuar@espol.edu.ec>
2025-12-04 13:46:00 -0500

    altered CORS policy

--- paganinibackend/src/main/java/espol/integradora/paganinibackend/config/WebConfig.java
+++ paganinibackend/src/main/java/espol/integradora/paganinibackend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
